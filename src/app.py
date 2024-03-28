805b4f11d8f6f3ae9122b575e9b13c3a5ce45f28
Tatsuhiro Terada <tatsuhirotera@gmail.com>
2024-03-28 14:53:37 +0900

    Rename Home

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 # streamlit を使用するために import します
 import streamlit as st
+import streamlit as st
+
+st.set_page_config(page_title="My_Projects_Home_Page", page_icon=":shark:", layout="wide")
 
 st.title('Home Page')
 
